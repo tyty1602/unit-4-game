612338fe0d31083361dd388a1514ed7591cca58e
tyty1602 <37279775+tyty1602@users.noreply.github.com>
2018-09-03 12:56:54 -0700

    files

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,14 @@
 //variables
 
-var targetScore = "";
+var targetScore = 0;
 var Diamond = "";
 var Pearl = "";
 var Ruby = "";
 var Emerald = "";
-var scorekeeper = "";
+var scorekeeper = 0;
 var win ="";
 var lose = "";
+var images =["assets/css/images/pearl.jpg", "assets/css/images/Ruby.jpg", "assets/css/images/emerald.jpg","assets/css/images/Diamond-thumbnail.jpg"]
 
 //Sanity check
 console.log("sanity check")
@@ -16,10 +17,43 @@ console.log("sanity check")
 
 function newGame() {
     targetScore = Math.floor(Math.random()* 84 + 11);
-    console.log(targetScore);
+    console.log("targetScore" + targetScore);
 }
 
-(newGame);
+function resetCrystal() {
+    for (var i =0; i< images.length; i++) {
+        var crystal = $("<img>");
+        crystal.addClass("crystal");
+        crystal.attr("src", images[i]);
+        crystal.attr("value",(Math.floor(Math.random() * 13) +1 ));
+        crystal.attr("height", "100");
+        $(".crystal").append(crystal)
+    }
+}
+
+function crystalClick() {
+    scoreKeeper+=parseInt($(this).attri("value"));
+    console.log("does CC work?")
+    $(".scorekeeper").html(scoreKeeper);
+    if (scoreKeeper===targetScore) {
+        wins++;
+        totalReset();
+    }
+    else if (scoreKeeper>targetScore) {
+        losses++;
+        totalReset();
+    }
+}
+
+function resetHTML() {
+    $(".targetScore").html(targetScore);
+}
+
+//page set up
+
+newGame();
+resetHTML();
+resetCrystal();
 
 //HTML links
 
