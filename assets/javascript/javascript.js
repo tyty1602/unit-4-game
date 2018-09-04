b486dbeac02a33d5edea46b029ce5b6ca32473bf
tyty1602 <37279775+tyty1602@users.noreply.github.com>
2018-09-03 18:49:19 -0700

    lala

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,65 +1,91 @@
 //variables
+$(document).ready(function () {
+    var targetScore = 0;
+    var Diamond = "";
+    var Pearl = "";
+    var Ruby = "";
+    var Emerald = "";
+    var scorekeeper = 0;
+    var wins = "";
+    var losses = "";
+    var counter = 0;
+    var images = ["assets/css/images/pearl.jpg", "assets/css/images/Ruby.jpg", "assets/css/images/emerald.jpg", "assets/css/images/Diamond-thumbnail.jpg"]
 
-var targetScore = 0;
-var Diamond = "";
-var Pearl = "";
-var Ruby = "";
-var Emerald = "";
-var scorekeeper = 0;
-var win ="";
-var lose = "";
-var images =["assets/css/images/pearl.jpg", "assets/css/images/Ruby.jpg", "assets/css/images/emerald.jpg","assets/css/images/Diamond-thumbnail.jpg"]
+    //Sanity check
+    console.log("sanity check")
 
-//Sanity check
-console.log("sanity check")
+    //NewGame
 
-//NewGame
-
-function newGame() {
-    targetScore = Math.floor(Math.random()* 84 + 11);
-    console.log("targetScore" + targetScore);
-}
-
-function resetCrystal() {
-    for (var i =0; i< images.length; i++) {
-        var crystal = $("<img>");
-        crystal.addClass("crystal");
-        crystal.attr("src", images[i]);
-        crystal.attr("value",(Math.floor(Math.random() * 13) +1 ));
-        crystal.attr("height", "100");
-        $(".crystal").append(crystal)
+    function newGame() {
+        targetScore = Math.floor(Math.random() * 184 + 11);
+        console.log("targetScore" + targetScore);
     }
-}
 
-function crystalClick() {
-    scoreKeeper+=parseInt($(this).attri("value"));
-    console.log("does CC work?")
-    $(".scorekeeper").html(scoreKeeper);
-    if (scoreKeeper===targetScore) {
-        wins++;
-        totalReset();
+
+    function resetCrystal() {
+        console.log("sanity check crystal img");
+        for (var i = 0; i < images.length; i++) {
+            var crystal = $("<img>");
+            crystal.addClass("crystal");
+            crystal.attr("src", images[i]);
+            crystal.attr("value", (Math.floor(Math.random() * 13) + 1));
+            console.log("crystal value");
+            crystal.attr("height", "100");
+            $(".crystalContainer").append(crystal);
+        }
     }
-    else if (scoreKeeper>targetScore) {
-        losses++;
-        totalReset();
+
+    function crystalClick () {
+		//attr returns first value of selected html element
+        counter += parseInt($(this).attr("value"));
+        console.log("parseint stff");
+        $(".scoreKeeper").html(counter);
+        console.log("parseint part2");
+		if (counter == targetScore) {
+			wins++;
+            totalReset();
+            console.log("counterworks?");
+		}
+		else if (counter > targetScore) {
+			losses++;
+			totalReset();
+		};
+	};
+
+    function resetHTML() {
+        $(".targetScore").html(targetScore);
+        $("win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+        $(".scoreKeeper").html(counter);
+		$(".crystalContainer").empty();
     }
-}
 
-function resetHTML() {
-    $(".targetScore").html(targetScore);
-}
+    //page set up
 
-//page set up
+    newGame();
+    resetHTML();
+    resetCrystal();
 
-newGame();
-resetHTML();
-resetCrystal();
 
-//HTML links
+    function totalReset () {
+		newGame ();
+		counter = 0;
+		resetHTML ();
+		resetCrystal ();
+	}
 
-function resetHTML () {
-    $("#targetScore").append(targetScore);
-}
 
-//Onclick
-$(document).on("click", ".crystal", crystalClick);
+    //HTML links
+
+    function resetHTML() {
+        $("#targetScore").append(targetScore);
+    }
+
+    //Onclick
+
+    $(".crystal").click(function() {
+        console.log("does onclick work?")
+
+    });
+  
+
+});
